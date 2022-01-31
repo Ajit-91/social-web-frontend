73af1d515a53157da0c5a01424ae8f159bf12bab
Ajit Kumar <ajitkumar12105@gmail.com>
2022-01-31 22:54:19 +0530

    resolved loading issue

--- frontend/src/Components/Loading.jsx
+++ frontend/src/Components/Loading.jsx
@@ -3,7 +3,7 @@ import { CircularProgress } from '@mui/material';
 
 
 const Loading = () => {
-  return <div style={{position : "absolute", top : "50%", right : "50%"}}>
+  return <div style={{position : "absolute", top : "50%", right : "50%", transform: "translate(50%, -50%)"}}>
       <CircularProgress size={60} thickness={3} />
   </div>;
 };
