42bf64585e21cf09fc9423bf84b7c95a69f9b762
Ajit Kumar <ajitkumar12105@gmail.com>
2022-02-05 16:31:24 +0530

    changed browserRouter to hashrouter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import {BrowserRouter} from "react-router-dom"
+import { HashRouter} from "react-router-dom"
 import store from './Redux/store';
 import App from "./App"
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename='social-web-frontend'>
+      <HashRouter >
           <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
